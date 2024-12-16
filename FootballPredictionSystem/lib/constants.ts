7cb913c76adf4bc75a7ecb9ad26f94627cf227d8
Winmix713 <takosadam@gmail.com>
2024-12-16 04:52:32 +0100

    Add files via upload

---
+++ FootballPredictionSystem/lib/constants.ts
@@ -0,0 +1,10 @@
+export const TEAMS = [
+  { id: "arsenal", name: "London Ágyúk", logoUrl: "https://resources.premierleague.com/premierleague/badges/50/t3.png", weight: 1.0, league: "premier-league" },
+  { id: "astonvilla", name: "Aston Oroszlán", logoUrl: "https://resources.premierleague.com/premierleague/badges/50/t7.png", league: "premier-league" },
+  // ... add all other teams here
+]
+
+export const MATCH_SLOTS = 8
+
+export const TIMER_DURATION = 375
+
